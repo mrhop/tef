b299630aeac795c6659d11bc67c07b198a906ea0
mrhop <233402923@qq.com>
2016-06-21 12:23:11 +0800

    Initial commit

---
+++ public/js/dev/self/basic.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Donghui Huo on 2016/3/15.
+ */
+var $ = jQuery = require('jquery');
+require("bootstrap");
+var utilFun = require("utilFun");
+$(document).ready(function () {
+   //do something
+});
+
